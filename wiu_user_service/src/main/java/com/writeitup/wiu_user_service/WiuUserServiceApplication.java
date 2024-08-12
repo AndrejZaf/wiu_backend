c0f82d522097f3e2b5b3490356d030ba5a7c56ef
AZafirovski <andrej.zafirovski@students.finki.ukim.mk>
2024-08-12 22:14:49 +0200

    Create user service placeholder

---
+++ wiu_user_service/src/main/java/com/writeitup/wiu_user_service/WiuUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.writeitup.wiu_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiuUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WiuUserServiceApplication.class, args);
+    }
+
+}
---
+++ wiu_user_service/src/test/java/com/writeitup/wiu_user_service/WiuUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.writeitup.wiu_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiuUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
